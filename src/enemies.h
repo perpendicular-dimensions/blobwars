f252549ec2a5392f2be8bb5ca707b27a464b4583
Guus Sliepen <guus@debian.org>
2010-12-30 15:53:29 +0100

    Update copyrights.
    The last release from Parallel Realities was in 2010.

--- src/enemies.h
+++ src/enemies.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2004 Parallel Realities
+Copyright (C) 2004-2010 Parallel Realities
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
